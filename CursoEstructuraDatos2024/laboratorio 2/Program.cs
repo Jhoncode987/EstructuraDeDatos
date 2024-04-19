3bff351760edc84ebfcce7b2c182647eaed34b9c
Jhoncode987 <jhonsosabellido987@gmail.com>
2024-04-19 11:57:21 -0500

    laboratorio 2

---
+++ CursoEstructuraDatos2024/laboratorio 2/Program.cs
@@ -0,0 +1,9 @@
+﻿// programa que sume los n primeros numeros
+var n = 10;
+var suma = 0;
+
+for (var i = 0; i <= n; i++) 
+{
+    suma = suma + i;
+}
+Console.WriteLine("el resultado de la suma es: "+ suma.ToString());
